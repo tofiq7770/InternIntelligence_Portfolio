220992a1252f5983344107945496b56e92b6e8b5
tofiq7770 <tofigtn@code.edu.az>
2024-12-14 22:40:19 +0400

    Initial

---
+++ Portfolio/DAL/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Portfolio.Models;
+
+namespace Portfolio.DAL
+{
+    public class AppDbContext : IdentityDbContext<AppUser>
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> opt) : base(opt)
+        {
+
+        }
+
+    }
+}
---
+++ Portfolio/Models/BaseEntity.cs
@@ -0,0 +1,10 @@
+﻿
+namespace Portfolio.Models
+{
+    public class BaseEntity
+    {
+        public int Id { get; set; }
+        public DateTime CreateDate { get; set; } = DateTime.Now;
+        public bool SoftDelete { get; set; } = false;
+    }
+}
